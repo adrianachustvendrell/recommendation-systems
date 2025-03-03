a38308cdaca18622bd4de87afbbb0b738bec6e89
valelopezch <57009786+valelopezch@users.noreply.github.com>
2025-03-03 02:19:45 +0100

    user update and redirect
    Se añade usuario nuevo al csv y se redirige a log in, si un usuario no tiene cuenta lo manda a registrarse y al ingresar correctamente va a la pagina principal.

--- hide_sidebar/pages/signin.py
+++ hide_sidebar/pages/signin.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import pandas as pd
 from pathlib import Path
+import time
 
 @st.cache_data
 def load_user_data():
@@ -9,13 +10,6 @@ def load_user_data():
     return pd.read_csv(file_path)  # Ensure the CSV file is in the correct directory
 
 user_data = load_user_data()
-
-# Check if user is already logged in
-if "authenticated" not in st.session_state:
-    st.session_state["authenticated"] = False
-
-# if st.session_state["authenticated"]:
-#     st.switch_page("recs_page.py")
 
 # Title for the sign-up page
 st.title("Inicia sesión")
@@ -26,8 +20,7 @@ with st.form(key="signin_form"):
     #st.subheader("Inicia Sesión")
 
     # User input fields
-    username = st.text_input("Enter your username")
-    password = st.text_input("Enter your password", type="password")
+    username = st.text_input("Ingresa tu nombre de usuario")
 
     # Submit button
     submit_button = st.form_submit_button(label="Submit")
@@ -39,16 +32,11 @@ if submit_button:
     user_row = user_data[user_data["nombre_usuario"] == username]
 
     if not user_row.empty:
-        # Extract user ID (password)
-        correct_password = str(user_row.iloc[0]["id_usuario"])
+        st.success(f"Welcome, {username}!")
+        time.sleep(2)
+        st.switch_page("app.py") 
 
-        if password == correct_password:
-            st.session_state["authenticated"] = True 
-            st.success(f"Welcome, {username}!")
-            # st.switch_page("pages/recs_page.py")
-            # st.switch_page("recs_page.py")
-        else:
-            st.error("Incorrect password. Please try again.")
     else:
-        st.error("Username not found. Please check your credentials.")
+        st.error("Usuario no encontrado. Por favor registrese y vuelva a iniciar sesión.")
+        st.page_link("pages/signup.py", label="👉 Regístrate aquí")
 
