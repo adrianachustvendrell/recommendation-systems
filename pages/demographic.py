4a3295d8bf2ff3a610df79c611e10c5b414cd2f0
ecanlar <evacantinlarumbe@gmail.com>
2025-03-11 19:09:35 +0100

    new files to recommendation types

---
+++ pages/demographic.py
@@ -0,0 +1,17 @@
+
+
+# TIPO 1 - viajero_disfrute (viajero promedio)
+tipo1 = {"Parques": 90, "Jardines botánicos": 80, "Playas": 80, "Lagos": 80, "Calles y plazas": 100, "Paseos": 100, "Parques temáticos": 90, "Cines": 90, "Teatros": 70, "Conciertos y música en vivo": 80, "Restaurantes": 100, "Eventos": 70, "Tiendas especializadas": 50, "Tiendas tradicionales": 60, "Exposiciones": 60, "Grandes eventos (exposiciones)": 70, "Nauticos": 60, "Eventos deportivos": 80, "Futbol": 90, "Baloncesto": 80, "Arte": 70, "Ciencia y tecnología": 60, "Historia": 50, "Religión": 40, "Ciencias naturales": 70, "Arqueologia": 50, "Historia y cultura local": 80, "Artesanía": 40, "Militar": 30, "Catedrales": 40, "Iglesias": 30, "Conventos": 20, "Campanarios": 30, "Cementerios": 10, "Tumbas": 10, "Criptas": 10, "Abadías": 20, "Basílica": 40, "Capilla": 30, "Colegiata": 20, "Castillos": 80, "Torres": 70, "Murallas": 60, "Puertas": 50, "Otras arquitecturas defensivas": 40, "Palacio": 70, "Casa": 50, "Centro histórico": 100, "Ateneo": 50, "Mercados": 100, "Edificios gubernamentales": 40, "Edificios académicos": 40, "Otros edificios públicos": 30, "Estaciones de transportes": 20, "Atarazanas": 30, "Puentes": 100, "Faros": 50, "Estadios y áreas deportivas": 90, "Esculturas": 70, "Fuentes": 90, "Centros de salud y spa": 40, "Otros salud y spa": 30, "Otros deportes": 80, "Otros espacios abiertos": 80, "Otros estilos": 50, "Otros ocio": 70, "Otros gastronomía": 90, "Otras compras": 80, "Otros eventos": 70, "Otros museos": 80, "Otros edificios emblemáticos": 50, "Otros monumentos": 40, "Otras arquitectura religiosa": 30}
+
+
+# TIPO 2 - viajero de borrachera y adultos con crisis de los 40
+tipo2 = {"Playas": 70, "Parques temáticos": 30, "Conciertos y música en vivo": 80, "Clubs y discotecas": 100, "Bares y pubs": 100, "Eventos": 80, "Fiestas": 100, "Eventos deportivos": 30, "Otros ocio": 40, "Otros eventos": 20}
+
+# TIPO 3 - viajero joven con cultura
+tipo3 = { "Museos": 95, "Arquitectura religiosa": 80, "Arquitectura civil": 85, "Centro histórico": 85, "Mercados": 75, "Edificios académicos": 95, "Monumentos": 80, "Esculturas": 90, "Historia y cultura local": 90, "Arqueología": 85, "Patrimonio de la Humanidad": 95, "Arte": 95, "Música clásica": 85, "Ópera": 90, "Paseos": 85, "Restaurantes": 90, "Otros gastronomía": 80, "Estilos y periodos": 80, "Exposiciones": 85, "Conferencias": 90, "Congresos": 90, "Grandes eventos (exposiciones)": 85, "Ciencia y tecnología": 90, "Historia": 90, "Religión": 75, "Ciencias naturales": 85, "Artesanía": 80, "Militar": 70, "Otros museos": 85, "Teatros": 90}
+
+tipo4 = { "Museos": 90, "Arquitectura religiosa": 85, "Cementerios": 85, "Arquitectura civil": 80, "Centro histórico": 85, "Mercados": 90, "Edificios gubernamentales": 80, "Otros edificios públicos": 80, "Otros edificios emblemáticos": 80, "Monumentos": 80, "Historia y cultura local": 90, "Patrimonio de la Humanidad": 95, "Música clásica": 80, "Ópera": 80, "Jardines botánicos": 75, "Calles y plazas": 85, "Paseos": 85, "Otros espacios abiertos": 75, "Restaurantes": 65, "Centros de salud y spa": 80}
+
+tipo5 = {"Parques": 90, "Jardines botánicos": 80, "Parque infantil": 100, "Playas": 80, "Lagos": 40, "Paseos": 90, "Parques temáticos": 60, "Ciencias naturales": 30, "Castillos": 50, "Torres": 40, "Murallas": 50, "Puertas": 50, "Otras arquitecturas defensivas": 30, "Otros espacios abiertos": 90}
+
+
