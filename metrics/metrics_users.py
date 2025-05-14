9b96f045c4c3699c95f10f0b645cd3d7e35430c9
ecanlar <evacantinlarumbe@gmail.com>
2025-05-14 12:21:38 +0200

    organize and claen code

--- metrics/metrics_users.py
+++ metrics/metrics_users.py
@@ -1,4 +1,4 @@
-from metrics import obtener_items_seleccionados
+from metrics.metrics import obtener_items_seleccionados
 import pandas as pd
 
 
@@ -9,9 +9,8 @@ import pandas as pd
 # SELECCIONAR LOS 20 PRIMEROS USUARIOS Y CALCULAR SUS ÍTEMS
 # ----------------------------------------------------------
 
-
-# seleccionar los primeros 20 usuarios
-df_usuarios = pd.read_csv('data/info_usuarios.csv')
+# Seleccionar los primeros 20 usuarios
+df_usuarios = pd.read_csv('../data/info_usuarios.csv')
 df = df_usuarios.loc[:19, ['id_usuario', 'nombre_usuario']]
 
 # Inicializar listas vacías
@@ -28,7 +27,6 @@ for _, row in df.iterrows():
     lista_colaborativo.append(obtener_items_seleccionados(["Colaborativo"], nombre))
     lista_hibrido.append(obtener_items_seleccionados(["Demográfico", "Basado en contenido", "Colaborativo"], nombre))
 
-# Asignar a columnas del DataFrame
 df['Demografico'] = lista_demografico
 df['Contenido'] = lista_contenido
 df['Colaborativo'] = lista_colaborativo
@@ -43,9 +41,8 @@ df['Híbrido'] = lista_hibrido
 # AÑADIR LAS PUNTUACIONES DE TEST
 # ----------------------------------------------------------
 
-
 # Leer el test set
-df_test = pd.read_csv("data/puntuaciones_usuario_test.csv")  
+df_test = pd.read_csv("../data/puntuaciones_usuario_test.csv")  
 
 # Asegurar que id_usuario sea del mismo tipo (por si acaso)
 df['id_usuario'] = df['id_usuario'].astype(int)
@@ -70,17 +67,13 @@ df['Test'] = lista_test
 
 
 
-
-
-
 # ----------------------------------------------------------
 # CALCULAR F1, RECALL Y PRECISIÓN CON UMBRALES
 # ----------------------------------------------------------
 
 # Umbrales
-# Umbrales
-umbral_recomendadores = 40  # Aumentar el umbral de recomendadores
-umbral_test = 65  # Aumentar el umbral de ítems relevantes
+umbral_recomendadores = 40 
+umbral_test = 65
 
 
 # Función para calcular las métricas de Precision, Recall, F1
@@ -101,32 +94,26 @@ def calcular_metricas(recomendados, relevantes):
 
 # Crear las columnas para las métricas
 def calcular_metricas_por_usuario(row):
-    # Obtener los ítems recomendados de los diferentes recomendadores
     recomendadores = {
         "Demografico": row['Demografico'],
         "Contenido": row['Contenido'],
         "Colaborativo": row['Colaborativo'],
-        "Híbrido": row['Híbrido']
-    }
+        "Híbrido": row['Híbrido']}
     
-    # Obtener los ítems del test
     test_items = row['Test']
     
-    # Calcular las métricas para cada recomendador
+    # Calcular las métricas (precisión, recall y f1) para cada recomendador
     metricas = {}
     for recomendador, recomendados in recomendadores.items():
-        # Calcular precisión, recall y f1 para cada recomendador
         precision, recall, f1 = calcular_metricas(recomendados, test_items)
         metricas[f"{recomendador}_precision"] = precision
         metricas[f"{recomendador}_recall"] = recall
         metricas[f"{recomendador}_f1"] = f1
-    
     return pd.Series(metricas)
+
 
 # Aplicar la función a cada fila del dataframe
 df = df.join(df.apply(calcular_metricas_por_usuario, axis=1))
-
-
 
 
 
@@ -148,7 +135,7 @@ def calcular_mae(recomendados, test_items, user_test_data):
             # Usamos el ratio real del test para este ítem
             ratio_real = test_items[item]
         else:
-            # Si no está en el test set, asignamos 0
+
             ratio_real = 0
         
         # Calculamos el error absoluto entre el ratio recomendado y el ratio real
@@ -163,15 +150,12 @@ def calcular_mae(recomendados, test_items, user_test_data):
 
 # Función para calcular el MAE para cada recomendador
 def calcular_mae_por_usuario(row):
-    # Obtener los ítems recomendados de los diferentes recomendadores
     recomendadores = {
         "Demografico": row['Demografico'],
         "Contenido": row['Contenido'],
         "Colaborativo": row['Colaborativo'],
-        "Híbrido": row['Híbrido']
-    }
+        "Híbrido": row['Híbrido']}
     
-    # Obtener los ítems del test para el usuario
     test_items = row['Test']
     
     # Calcular el MAE para cada recomendador
@@ -186,9 +170,6 @@ def calcular_mae_por_usuario(row):
 df = df.join(df.apply(calcular_mae_por_usuario, axis=1))
 
 
-
-
-
 # Guardar el DataFrame en un archivo CSV
-df.to_csv('data/metricas_usuarios.csv', index=False)
+df.to_csv('../data/metricas_usuarios.csv', index=False)
 
